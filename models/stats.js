3f1ca30b7e564408477f1e43e44199b48e770fcc
SamH1501 <sh175@hw.ac.uk>
2020-11-22 21:45:57 +0000

    database started

---
+++ models/stats.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    browser: {
+        type: Number,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
+    },
+    highScore: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    totalEaten: {
+        type: Number,
+        default: 0,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

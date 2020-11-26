2b7f572db83c8d8cc3b3d38d40b7968c53a6cc0c
unknown <georgekitsios99@gmail.com>
2020-11-26 02:01:52 +0000

    room start

--- index.js
+++ index.js
@@ -7,17 +7,62 @@ const { playerJoin, getPlayers, playerLeave, getPlayer, updatePosition } = requi
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const gameRooms = ["room1","room2","room3","room4"];
+var players = [];
+var room = []
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', socket => {
-    socket.on('joinGame', ({ name }) => {
-        playerJoin(socket.id, name);
+io.of("/namespace").on("connection" , (socket) => {
+    console.log("New Client");
+    io.emit("welcome" , "Hello and Welcome");
 
+    socket.on("joinRoom" , (room) => {
+        if(gameRooms.includes(room)) {
+            io.emit('playerJoin', socket.id, name , room);
+            return socket.emit("success" , "You have joined room" + room);
+        } else {
+            return socket.emit("err" , "No Room named " + room);
+        }
+})
+})
+
+// socket.on('create or join', function (room) {
+    
+    //var numClients = io.sockets.clients(room).length;
+io.on('connection', socket =>{
+    socket.on('joinGame', ({ name , room }) => { 
+    if (socket.id === 0){
+        io.emit('playerJoin', socket.id, name , room);
+        io.emit('created', room, socket.id);
+
+    } else if (socket.id === 1) {
+        io.emit('playerJoin',socket.id, name , room);
+        io.emit('joined', room, socket.id);
+        io.sockets.in(room).emit('ready');
+
+    } else { // max two clients
+        socket.on('disconnect', () => {
+            playerLeave(socket.id); 
+            })
+        
+    }
+});
+});
+ 
+const state = {};
+const clientRooms = {};
+
+io.on('connection', socket => {
+    socket.on('joinGame', ({ name , room }) => {
+        
+        playerJoin(socket.id, name , room);
+        
         io.emit('updatePlayers', {
-            players: getPlayers()
+          players: getPlayers()
         })
         io.emit('getPlayer', { playerData: getPlayer(socket.id) })
+    
     })
     
     socket.on('movePlayer', ({ direction }) => {
@@ -30,20 +75,17 @@ io.on('connection', socket => {
             players: getPlayers()
         })
         io.emit('getPlayer', { playerData: getPlayer(socket.id) })
+   
     })
 
-    
-
-    
-    
     socket.on('disconnect', () => {
         playerLeave(socket.id);
         io.emit('updatePlayers', {
             players: getPlayers()
+        
         })
+    
     });
 });
-
-
 
 server.listen(3000);

4a853d3dce58f1bf2c5f98212ff030d2c1dfed12
katie mcaskill <katie.mcaskill632@gmail.com>
2020-11-29 16:43:44 +0000

    updated leaderboard

---
+++ public/js/leaderboard-client.js
@@ -0,0 +1,19 @@
+function makeTableHTML(myArray) {
+
+    let result = "<table border=1>";
+
+    let highscore = [{name: "jason", score :0}, {name: "sam", score :0}, {name: "george", score :2}, {name: "katie", score :1000000000}]
+    
+
+    for(let i=0; i<highscore.length; i++) {
+
+        result += "<tr>";
+
+
+       
+
+    }
+
+    result += "</table>";
+    return result;
+}

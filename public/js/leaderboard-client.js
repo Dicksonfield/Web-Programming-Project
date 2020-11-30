30e911c2ce95bd5c59ec5c5a8a93efd9bf8fcc22
katie mcaskill <katie.mcaskill632@gmail.com>
2020-11-30 23:45:56 +0000

    completed leaderboard

--- public/js/leaderboard-client.js
+++ public/js/leaderboard-client.js
@@ -1,23 +1,21 @@
 const socket = io();
 
-function makeTableHTML(myArray) {
-
-    let result = "<table border=1>";
-
-    let highscore = [{name: "jason", score :0}, {name: "sam", score :0}, {name: "george", score :2}, {name: "katie", score :1000000000}]
-    
-
-    for(let i=0; i<highscore.length; i++) {
-
-        result += "<tr> <td>name[i]</td> <td>score[0]</td> </tr>";       
-
-    }
-
-    result += "</table>";
-    return result;
-}
-
 socket.emit("requestLeaderboard");
 socket.on("sendLeaderboard", ({leaderboard}) => {
     if(leaderboard.length > 100) leaderboard.slice(0,100);
+    makeTable(leaderboard);
 });
+
+function makeTable(name){
+
+    let result = "";
+    result = "<table><th>Place</th><th>Name</th><th>Highscore</th><th>Total Eaten</th><th>Wins</th>";
+
+    for(let i=0; i<name.length; i++) {
+
+        result += `<tr> <td>${i+1}</td><td>${name[i].user}</td> <td>${name[i].highScore}</td> <td>${name[i].totalEaten}</td> <td>${name[i].wins}</td></tr>`;
+    }
+
+    result += "</table>";       
+    leaderboard.innerHTML=result;
+}

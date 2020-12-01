55ae0d148be9ebeda4f26652f7de0214acc0b4aa
katie mcaskill <katie.mcaskill632@gmail.com>
2020-12-01 18:04:38 +0000

    updated leaderboard scrolling

--- public/js/leaderboard-client.js
+++ public/js/leaderboard-client.js
@@ -1,8 +1,7 @@
 const socket = io();
-
 socket.emit("requestLeaderboard");
 socket.on("sendLeaderboard", ({leaderboard}) => {
-    if(leaderboard.length > 100) leaderboard.slice(0,100);
+    if(leaderboard.length > 100) leaderboard = leaderboard.slice(0,100);
     makeTable(leaderboard);
 });
 
